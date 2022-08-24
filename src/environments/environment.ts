8aa4d65d1e6c4175bac56582f08a579aa5b5a245
Snehesh Dutta <sneheshdutta@gmail.com>
2022-08-24 12:13:47 +0530

    added new deploy LB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/',
-  register: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/login/register/',
-  login: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/login/authenticate/',
-  processPension: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/ProcessPension/',
-  pensionDetails: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/ProcessPension/'
+  apiUrl: 'http://digi-pms-lb-1473065883.ap-south-1.elb.amazonaws.com/',
+  register: 'http://digi-pms-lb-1473065883.ap-south-1.elb.amazonaws.com/login/register/',
+  login: 'http://digi-pms-lb-1473065883.ap-south-1.elb.amazonaws.com/login/authenticate/',
+  processPension: 'http://digi-pms-lb-1473065883.ap-south-1.elb.amazonaws.com/ProcessPension/',
+  pensionDetails: 'http://digi-pms-lb-1473065883.ap-south-1.elb.amazonaws.com/ProcessPension/'
 };
 
 /*
